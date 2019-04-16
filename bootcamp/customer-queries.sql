f8526c1459ce5afea9fac4f81c0a73f5cfa40031
mccanndt <mccanndt91@gmail.com>
2019-04-16 16:13:11 -0400

    Added an alias query

--- bootcamp/customer-queries.sql
+++ bootcamp/customer-queries.sql
@@ -1,1 +1,3 @@
-SELECT * FROM customer WHERE sales > 30000000 AND sales < 70000000 order by sales desc
+SELECT * FROM customer WHERE sales > 30000000 AND sales < 70000000 order by sales desc;
+
+SELECT Name as 'Customer Name', Sales as 'Total Sales' FROM customer;
